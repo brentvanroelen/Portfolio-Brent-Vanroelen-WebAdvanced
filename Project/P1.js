d786b47d6182d7e442bc1d9fbbfed9841b3ff554
brentvanroelen <145969821+brentvanroelen@users.noreply.github.com>
2024-03-06 18:31:13 +0100

    06/03
    Geprobeerd punt 4 te voltooien, moet nog wat extra opzoek werk doen.

--- Project/P1.js
+++ Project/P1.js
@@ -1,6 +1,19 @@
 let title1 = document.getElementById('h1');
 let tile2 = document.getElementById('h2');
 
+function formChecker(){
+    let mijnForm = document.getElementById('mijnForm').value;
+
+    if(mijnForm == null){
+        alert('Hey, you have to choose your favorite character!!');
+        return false;
+    }
+}
+
+
+
+
+
 let newTitle2 = document.createTextNode(tile2.textContent = 'Zuko');
 
 
