8fa21572aebc763f2a443e00fe503df9f2c3cacf
brentvanroelen <145969821+brentvanroelen@users.noreply.github.com>
2024-03-09 18:21:17 +0100

    Bewerkingen van 09/03
    form validation is gelukt+ leuke foto toegevoegd en puntje van constante

--- Project/P1.js
+++ Project/P1.js
@@ -1,22 +1,47 @@
+'use strict';
+
+//vars
 let title1 = document.getElementById('h1');
-let tile2 = document.getElementById('h2');
+let title2 = document.getElementById('h2');
 
-function formChecker(){
-    let mijnForm = document.getElementById('mijnForm').value;
+//DOM manipulatie
+let newTitle2 = document.createTextNode(title2.textContent = 'Zuko');
 
-    if(mijnForm == null){
+//Event
+title1.addEventListener('click', function(){
+    let newTitle1 = document.createTextNode(title1.textContent = 'The Legend Of Korra');
+});
+
+//form valideren
+function formChecker(event){
+    event.preventDefault();
+    let mijnForm = document.getElementById('mijnForm');
+    let geselecteerdeWaarde = mijnForm.value;
+
+    if(geselecteerdeWaarde == null || geselecteerdeWaarde == '' ){
         alert('Hey, you have to choose your favorite character!!');
         return false;
     }
 }
 
 
+let formElement = document.getElementById('mijnForm');
+formElement.addEventListener('submit', formChecker);
+
+
+//Constante
+
+const leeftijdAang = 112;
+
+let AangleeftijdUitIjs = 12; 
+
+let AantalJarenInIjs = leeftijdAang - AangleeftijdUitIjs;
 
 
 
-let newTitle2 = document.createTextNode(tile2.textContent = 'Zuko');
+let revealAntwoord = document.getElementById('antwoord');
 
-
-title1.addEventListener('click',function(){
-    let newTitle1 = document.createTextNode(title1.textContent = 'The Legend Of Korra')
+revealAntwoord.addEventListener('click',function(){
+    let correctAntwoord = document.createTextNode(revealAntwoord.textContent = `De time Aang was in the ice berg is: ${AantalJarenInIjs}` );
 })
+
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
  
 Bij puntje 1,2&3 heb ik de HTML-info gehaald van ChatGPT voor inspiratie. https://chat.openai.com/share/dc8d6f87-2540-4aa2-961c-0e1ff51aca03 
 
-Bij puntje 4 heb ik gebruik gemaakt van nog wat info gegenereerd door chatgpt: https://chat.openai.com/share/b0f6609b-5aac-4949-bedc-33ff77aa429e
+Bij puntje 4 heb ik gebruik gemaakt van nog wat info gegenereerd door chatgpt: https://chat.openai.com/share/b0f6609b-5aac-4949-bedc-33ff77aa429e, heb ook gebruikt gemaakt van de event.preventDefault()functie van de site quora.com.
