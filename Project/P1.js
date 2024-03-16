90a790755617efdfe6d61c7ad27a8e11ae1a5f5f
brentvanroelen <145969821+brentvanroelen@users.noreply.github.com>
2024-03-16 12:11:27 +0100

    werk 16/03

--- Project/P1.js
+++ Project/P1.js
@@ -15,15 +15,20 @@ title1.addEventListener('click', function(){
 //form valideren
 function formChecker(event){
     event.preventDefault();
-    let mijnForm = document.getElementById('mijnForm');
-    let geselecteerdeWaarde = mijnForm.value;
+    
+    let mijnForm = document.getElementsByClassName('MyLittleForm');
+    let ingevoerdeWaarde = document.getElementById('favCharacter').value;
 
-    if(geselecteerdeWaarde == null || geselecteerdeWaarde == '' ){
+    if(ingevoerdeWaarde == null || ingevoerdeWaarde == '' ){
         alert('Hey, you have to choose your favorite character!!');
         return false;
+    }else{
+        alert('You have selected: ' + ingevoerdeWaarde + ' as your favorite character');
     }
 }
 
+
+//Event
 
 let formElement = document.getElementById('mijnForm');
 formElement.addEventListener('submit', formChecker);
@@ -45,3 +50,33 @@ revealAntwoord.addEventListener('click',function(){
     let correctAntwoord = document.createTextNode(revealAntwoord.textContent = `De time Aang was in the ice berg is: ${AantalJarenInIjs}` );
 })
 
+// template literals
+
+let nameRoku = 'Avatar Roku'
+let ageRoku = 70
+
+console.log(`My name is ${nameRoku} and I am ${ageRoku} years old`)
+
+
+
+
+//destructuring
+
+let pSubbending = document.getElementById('pSubbending');
+
+const subBending = {
+    earthbending: 'lavabending, seismic sense, metalbending',
+    airbending: 'spiritual projection, flying, soundbending',
+    waterbending: 'bloodbending, healing, plantbending',
+    firebending: 'lightning, combustion, blue fire'
+}
+
+differentBending(subBending)
+
+function differentBending({earthbending, airbending, waterbending, firebending}){
+
+    let newPSubbending = document.createTextNode(pSubbending.textContent = `Some of the different bending techniques are, for erthbending: ${earthbending}, the airnation could:  ${airbending}, in the watertribes the poeple could: ${waterbending} and the ruthless firenation made use of: ${firebending}`);
+    
+}
+
+differentBending(subBending)
--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Bij puntje 1,2&3 heb ik de HTML-info gehaald van ChatGPT voor inspiratie. https://chat.openai.com/share/dc8d6f87-2540-4aa2-961c-0e1ff51aca03 
 
 Bij puntje 4 heb ik gebruik gemaakt van nog wat info gegenereerd door chatgpt: https://chat.openai.com/share/b0f6609b-5aac-4949-bedc-33ff77aa429e, heb ook gebruikt gemaakt van de event.preventDefault()functie van de site quora.com.
+
+Heb gebruik gemaakt van bootstrap.com voor mijn html een beetje mooier te maken, ook heb ik op ws3school extra info opgezocht over destructuring.
